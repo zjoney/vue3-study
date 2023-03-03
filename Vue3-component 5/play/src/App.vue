496b4643a2fb34c3fac06d28b4d41c92416f2d1f
zjoney <zdjoey@126.com>
2023-03-03 13:59:17 +0800

    feat: 🎸 实现组件play环境

---
+++ Vue3-component 5/play/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { UploadRawFile } from '@zi-shui/components/upload/src/upload';
+
+  const handleBeforeUpload = (rawFile:UploadRawFile)=>{
+
+    // return false;
+    return true
+  }
+</script>
+
+<template>
+  <z-upload multiple :before-upload="handleBeforeUpload" action="http://localhost:4000/upload" drag>
+    <z-button>点我上传</z-button>
+  </z-upload>
+</template>
