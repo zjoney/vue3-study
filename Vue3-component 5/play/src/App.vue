c3e437582afbb1131f69a6c600f4a4babd39b33f
zhangjing520 <zdjoey@126.com>
2023-03-14 08:22:37 +0800

    feat: 🎸 icon自定义组件/增加模板解析

--- Vue3-component 5/play/src/App.vue
+++ Vue3-component 5/play/src/App.vue
@@ -1,4 +1,11 @@
 <script setup lang="ts">
+/**
+  - 安装icon方式：https://github.com/07akioni/xicons
+  - 网站：https://www.xicons.org/#/
+  - 命令：npm i -D @vicons/ionicons5
+  - import { AddCircle} from '@vicons/ionicons5'
+  
+ */
 import { UploadRawFile } from '@zi-shui/components/upload/src/upload';
 
   const handleBeforeUpload = (rawFile:UploadRawFile)=>{
@@ -9,6 +16,11 @@ import { UploadRawFile } from '@zi-shui/components/upload/src/upload';
 </script>
 
 <template>
+<!--  使用方式
+  <z-icon :color="'red'" :size="20">
+    <AddCircle></AddCircle>
+    </z-icon>
+-->
   <z-upload multiple :before-upload="handleBeforeUpload" action="http://localhost:4000/upload" drag>
     <z-button>点我上传</z-button>
   </z-upload>
